395d2e2517e43aa6cf1378a0b751c5abd011cfc5
Dustin Hurtt <dustin_hurtt@outlook.com>
2022-03-23 13:52:19 +0000

    Add: 8 kyu Volume of a Cuboid (58261acb22be6e2ed800003a)

---
+++ javascript/8kyu/volume-of-a-cuboid.js
@@ -0,0 +1,9 @@
+// Codewars: Volume of a Cuboid
+// Link: https://www.codewars.com/kata/58261acb22be6e2ed800003a
+// Language: javascript
+// Kyu: 8 kyu
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    return length * width * height
+  }
+}
