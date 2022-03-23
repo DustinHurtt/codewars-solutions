0b2f6ef0ef5e650e865a1fc8fd786d4cd04c09c7
Dustin Hurtt <dustin_hurtt@outlook.com>
2022-03-23 13:41:06 +0000

    Add: 8 kyu Multiply (50654ddff44f800200000004) [go]

---
+++ go/8kyu/multiply.go
@@ -0,0 +1,9 @@
+// Codewars: Multiply
+// Link: https://www.codewars.com/kata/50654ddff44f800200000004
+// Language: go
+// Kyu: 8 kyu
+package multiply
+
+func Multiply(a, b int) int {
+  return a * b
+}
