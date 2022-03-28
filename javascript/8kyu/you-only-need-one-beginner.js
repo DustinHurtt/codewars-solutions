9a334ca6d1288968f08bcd88ec4eb10b5fbcd7b7
Dustin Hurtt <dustin_hurtt@outlook.com>
2022-03-28 13:06:22 +0000

    Add: 8 kyu You only need one - Beginner (57cc975ed542d3148f00015b)

---
+++ javascript/8kyu/you-only-need-one-beginner.js
@@ -0,0 +1,7 @@
+// Codewars: You only need one - Beginner
+// Link: https://www.codewars.com/kata/57cc975ed542d3148f00015b
+// Language: javascript
+// Kyu: 8 kyu
+function check(a, x) {
+  return a.includes(x)
+}
