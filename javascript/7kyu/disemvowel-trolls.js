33f73fc71acfc5656aaadc73110a8b88bf7f8640
Dustin Hurtt <dustin_hurtt@outlook.com>
2022-04-13 13:47:29 +0000

    Add: 7 kyu Disemvowel Trolls (52fba66badcd10859f00097e)

---
+++ javascript/7kyu/disemvowel-trolls.js
@@ -0,0 +1,9 @@
+// Codewars: Disemvowel Trolls
+// Link: https://www.codewars.com/kata/52fba66badcd10859f00097e
+// Language: javascript
+// Kyu: 7 kyu
+function disemvowel(str) {
+  
+ let newString = str.replace(/[aeiou]/gi, '');
+  return newString
+}
