9a3414e6f00483a3c241a392e7878c45d66e498d
Dustin Hurtt <dustin_hurtt@outlook.com>
2023-02-02 14:16:42 +0000

    Add: 8 kyu Grasshopper - Summation (55d24f55d7dd296eb9000030)

---
+++ javascript/8kyu/grasshopper-summation.js
@@ -0,0 +1,7 @@
+// Codewars: Grasshopper - Summation
+// Link: https://www.codewars.com/kata/55d24f55d7dd296eb9000030
+// Language: javascript
+// Kyu: 8 kyu
+var summation = function (num) {
+   return (num * (num + 1)) / 2
+}
