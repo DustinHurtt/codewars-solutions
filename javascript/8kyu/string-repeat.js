369fff09119fe51c54888b099fd594d1d02a50fa
Dustin Hurtt <dustin_hurtt@outlook.com>
2023-02-10 14:04:24 +0000

    Add: 8 kyu String repeat (57a0e5c372292dd76d000d7e)

---
+++ javascript/8kyu/string-repeat.js
@@ -0,0 +1,7 @@
+// Codewars: String repeat
+// Link: https://www.codewars.com/kata/57a0e5c372292dd76d000d7e
+// Language: javascript
+// Kyu: 8 kyu
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
