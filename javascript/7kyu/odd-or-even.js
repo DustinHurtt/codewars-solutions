4183e2b29d8440e68292c3ee9abddaf4e4784f10
Dustin Hurtt <dustin_hurtt@outlook.com>
2023-04-26 13:40:55 +0000

    Add: 7 kyu Odd or Even? (5949481f86420f59480000e7)

---
+++ javascript/7kyu/odd-or-even.js
@@ -0,0 +1,9 @@
+// Codewars: Odd or Even?
+// Link: https://www.codewars.com/kata/5949481f86420f59480000e7
+// Language: javascript
+// Kyu: 7 kyu
+function oddOrEven(array) {
+  
+  return array.reduce((a, b) => a + b, 0) % 2 === 0 ? 'even' : 'odd'
+
+}
