2a2b092dd26b651b88224d23e66be9f152626392
Dustin Hurtt <dustin_hurtt@outlook.com>
2023-06-20 13:14:50 +0000

    Add: 8 kyu Convert a Number to a String! (5265326f5fda8eb1160004c8)

---
+++ javascript/8kyu/convert-a-number-to-a-string.js
@@ -0,0 +1,7 @@
+// Codewars: Convert a Number to a String!
+// Link: https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+// Language: javascript
+// Kyu: 8 kyu
+function numberToString(num) {
+  return String(num)
+}
