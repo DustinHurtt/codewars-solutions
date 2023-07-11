0eceaede7beb43a208df8e50939902839f57ecbe
Dustin Hurtt <dustin_hurtt@outlook.com>
2023-07-11 18:13:07 +0000

    Add: 7 kyu Remove the minimum (563cf89eb4747c5fb100001b) [javascript]

--- javascript/7kyu/remove-the-minimum.js
+++ javascript/7kyu/remove-the-minimum.js
@@ -4,10 +4,9 @@
 // Kyu: 7 kyu
 function removeSmallest(numbers) {
 
-  let mapped = [...numbers].map((number, i) => {
-                              return {rating: number, position: i}
-                                })
-  let sorted = mapped.sort((a, b) => a.rating - b.rating)
-
-  return [...numbers].filter((element, index) => index !== sorted[0].position)
+  let newArr = [...numbers]
+  
+  newArr.splice(newArr.indexOf(Math.min(...newArr)), 1)
+  
+  return newArr
 }
