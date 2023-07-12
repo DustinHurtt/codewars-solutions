fe2f103d06a943b1ac9956a02839f3e6b4f25bf5
Dustin Hurtt <dustin_hurtt@outlook.com>
2023-07-12 13:32:49 +0000

    Add: 7 kyu Testing 1-2-3 (54bf85e3d5b56c7a05000cf9)

---
+++ javascript/7kyu/testing-1-2-3.js
@@ -0,0 +1,7 @@
+// Codewars: Testing 1-2-3
+// Link: https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9
+// Language: javascript
+// Kyu: 7 kyu
+var number=function(array){
+  return array.map((element, index) => `${index+1}: ${element}`)
+}
