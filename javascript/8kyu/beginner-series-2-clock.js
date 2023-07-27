b67a981aca4e89200f1aa38040c5a4b727232dd9
Dustin Hurtt <dustin_hurtt@outlook.com>
2023-07-27 19:53:03 +0000

    Add: 8 kyu Beginner Series #2 Clock (55f9bca8ecaa9eac7100004a)

---
+++ javascript/8kyu/beginner-series-2-clock.js
@@ -0,0 +1,7 @@
+// Codewars: Beginner Series #2 Clock
+// Link: https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a
+// Language: javascript
+// Kyu: 8 kyu
+function past(h, m, s){
+  return (h * 60 * 60 + m * 60 + s) * 1000
+}
