c16bbff9797fc340989becec39b1bc86d6aec75d
Dustin Hurtt <dustin_hurtt@outlook.com>
2023-08-25 05:36:41 +0000

    Add: 4 kyu Snail (521c2db8ddc89b9b7a0000c1) [javascript]

--- javascript/4kyu/snail.js
+++ javascript/4kyu/snail.js
@@ -3,63 +3,99 @@
 // Language: javascript
 // Kyu: 4 kyu
 snail = function(array) {
+  
+  let copiedArray = [...array]
+  
+  let newArray = []
+  
+  let counter = 0
+  
+function populateArray(arr) {
+  
+  
+  while (arr.length) {
+      
+    switch(counter) {
+        case 0: 
+//           console.log("ARRRR", arr)
+          newArray.push(...arr[0])
+          arr.shift()
 
-  let newArray = [];
+          if(arr.length) {
+            counter++
+          }
+          break;   
+        
+        case 1: 
+        
+            for (let i = 0; i < arr.length; i++) {
+              
+//               arr[i].forEach((element, i, arr) => {
+                     
+//               newElement()
+//               })
+              
+              newArray.push(arr[i][arr[i].length-1])
 
-  let counter = 0;
+//               arr[i].forEach((element, i, arr) => {
+//                 console.log("ELEM", element)
+//                 console.log(arr[arr.length-1])
+//                 newArray.push(arr[arr.length-1])
+                 
+//               })
+              arr[i].pop()
+            }
 
-  function populateArray(arr) {
-    while (arr.length) {
-      switch (counter) {
-        case 0:
-          newArray.push(...arr[0]);
-          arr.shift();
-
-          if (arr.length) {
-            counter++;
+//             arr.forEach((element, i, arr) => {
+              
+//               element.forEach((el, i, arr) => {
+//                 newArray.push(arr[arr.length-1])
+//               })
+//                 arr.pop()
+                
+                
+//             })
+      
+          if(arr.length) {
+            counter++
           }
           break;
-
-        case 1:
-          for (let i = 0; i < arr.length; i++) {
-            newArray.push(arr[i][arr[i].length - 1]);
-
-            arr[i].pop();
-          }
-
-          if (arr.length) {
-            counter++;
-          }
-          break;
-
+        
         case 2:
-          arr[arr.length - 1].reverse();
-          arr[arr.length - 1].forEach((element) => {
-            newArray.push(element);
-          });
-          arr.pop();
+        
+        arr[arr.length-1].reverse()
+        arr[arr.length-1].forEach((element) => {
+          newArray.push(element)
+        })
+        arr.pop()
+        
+        if(arr.length) {
+          counter++
+        }
+          break;   
+        
+        case 3: 
 
-          if (arr.length) {
-            counter++;
-          }
-          break;
-
-        case 3:
           for (let i = arr.length - 1; i >= 0; i--) {
-            newArray.push(arr[i][0]);
-            arr[i].shift();
-          }
-
-          counter = 0;
-
-          break;
+            
+            newArray.push(arr[i][0])
+            arr[i].shift()
       }
-
-      populateArray(arr);
+        
+        
+          counter = 0
+          
+          break;
     }
+    
+    populateArray(arr)
+
   }
-
-  populateArray(array);
-
-  return newArray;
+  
+}
+    
+  populateArray(copiedArray)
+      console.log("New", newArray)
+  
+return newArray
 }
