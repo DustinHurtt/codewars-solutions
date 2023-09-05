53f436c8a041f23ce2fe8f13cf734a092e1e84fc
Dustin Hurtt <dustin_hurtt@outlook.com>
2023-09-05 13:36:18 +0000

    Add: 8 kyu Opposite number (56dec885c54a926dcd001095)

---
+++ javascript/8kyu/opposite-number.js
@@ -0,0 +1,7 @@
+// Codewars: Opposite number
+// Link: https://www.codewars.com/kata/56dec885c54a926dcd001095
+// Language: javascript
+// Kyu: 8 kyu
+function opposite(number) {
+  return number/-1
+}
