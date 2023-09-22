c61ebc80cc8ebb7321bcaa1f6159f330319e1a8e
Dustin Hurtt <dustin_hurtt@outlook.com>
2023-09-22 13:38:53 +0000

    Add: 7 kyu Jaden Casing Strings (5390bac347d09b7da40006f6) [javascript]

--- javascript/7kyu/jaden-casing-strings.js
+++ javascript/7kyu/jaden-casing-strings.js
@@ -3,16 +3,6 @@
 // Language: javascript
 // Kyu: 7 kyu
 String.prototype.toJadenCase = function () {
- let returnString = ''
-  for (let i = 0; i < this.length; i++) {
-    if (i === 0) {
-      returnString += this[i].toUpperCase()
-    } else if (this[i] === ' ' && this[i + 1]) {
-      returnString += this[i] + this[i + 1].toUpperCase()
-      
-    } else if (this[i-1] !== ' ') {
-      returnString += this[i]
-    }
-  }
-  return returnString
+    return this.split(" ").map((word) => word.charAt(0).toUpperCase() + word.slice(1, word.length)).join(" ")
 };
+
