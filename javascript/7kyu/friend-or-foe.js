914415b7eab83e32d06dc56715752c550e19246d
Dustin Hurtt <dustin_hurtt@outlook.com>
2023-10-09 13:19:45 +0000

    Add: 7 kyu Friend or Foe? (55b42574ff091733d900002f)

---
+++ javascript/7kyu/friend-or-foe.js
@@ -0,0 +1,7 @@
+// Codewars: Friend or Foe?
+// Link: https://www.codewars.com/kata/55b42574ff091733d900002f
+// Language: javascript
+// Kyu: 7 kyu
+function friend(friends){
+  return friends.filter((word) => word.length === 4)
+}
