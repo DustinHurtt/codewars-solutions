66d1f4d7aa45625f9b049e4475cf4e4e1653d3cb
Dustin Hurtt <dustin_hurtt@outlook.com>
2023-11-13 15:07:34 +0000

    Add: 8 kyu Reversed Strings (5168bb5dfe9a00b126000018)

---
+++ javascript/8kyu/reversed-strings.js
@@ -0,0 +1,7 @@
+// Codewars: Reversed Strings
+// Link: https://www.codewars.com/kata/5168bb5dfe9a00b126000018
+// Language: javascript
+// Kyu: 8 kyu
+function solution(str){
+  return str.split('').reverse().join('')
+}
