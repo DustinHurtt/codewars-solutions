4fc8ac6888acbf70855e15edb359dc73c48596f5
Dustin Hurtt <dustin_hurtt@outlook.com>
2024-03-18 15:10:46 +0000

    Add: 6 kyu Who likes it? (5266876b8f4bf2da9b000362) [javascript]

--- javascript/6kyu/who-likes-it.js
+++ javascript/6kyu/who-likes-it.js
@@ -3,26 +3,22 @@
 // Language: javascript
 // Kyu: 6 kyu
 function likes(names) {
-  let statement
-  
-  switch(true) {
-      case names.length === 0:
-      statement = 'no one likes this'
-      break;
-      case names.length === 1:
-      statement = `${names[0]} likes this`
-      break
-      case names.length === 2:
-      statement = `${names[0]} and ${names[1]} like this`
-      break
-      case names.length === 3:
-      statement = `${names[0]}, ${names[1]} and ${names[2]} like this`
-      break
-      case names.length > 3:
-      statement = `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
-      
+
+  switch(names.length) {
+      case 0 :      
+      return 'no one likes this'
+        break
+      case 1 :       
+      return `${names[0]} likes this`
+        break
+      case 2 :
+        return `${names[0]} and ${names[1]} like this`
+        break
+      case 3 :
+        return `${names[0]}, ${names[1]} and ${names[2]} like this`
+        break
+      default :
+        return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
   }
-  
-  return statement
-  // TODO
+
 }
