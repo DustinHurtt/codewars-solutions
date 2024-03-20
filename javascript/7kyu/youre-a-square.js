0219af68b8039305dbf55946da04fbbdce102212
Dustin Hurtt <dustin_hurtt@outlook.com>
2024-03-20 13:32:40 +0000

    Add: 7 kyu You're a square! (54c27a33fb7da0db0100040e)

---
+++ javascript/7kyu/youre-a-square.js
@@ -0,0 +1,7 @@
+// Codewars: You're a square!
+// Link: https://www.codewars.com/kata/54c27a33fb7da0db0100040e
+// Language: javascript
+// Kyu: 7 kyu
+var isSquare = function(n){
+  return Number.isInteger(Math.sqrt(n)); // fix me
+}
