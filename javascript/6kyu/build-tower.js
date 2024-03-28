ee1e2b602de81c86481044c258c79e381fbb5695
Dustin Hurtt <dustin_hurtt@outlook.com>
2024-03-28 19:35:33 +0000

    Add: 6 kyu Build Tower (576757b1df89ecf5bd00073b) [javascript]

--- javascript/6kyu/build-tower.js
+++ javascript/6kyu/build-tower.js
@@ -3,5 +3,17 @@
 // Language: javascript
 // Kyu: 6 kyu
 function towerBuilder(nFloors) {
-   return ' '.repeat(nFloors).split('').map((el, i) => `${' '.repeat(nFloors - i - 1)}${'*'.repeat(2 * i + 1)}${' '.repeat(nFloors - i - 1)}`)
+  let pyramid = []
+  for (let i = 0; i < nFloors; i++) {
+    let asterisksNum = 2 * i + 1
+    let padding = ' '.repeat(nFloors - 1 - i)
+    let asterisks = '*'.repeat(asterisksNum)
+    pyramid.push(`${padding}${asterisks}${padding}`)
+  }
+  return pyramid
+  
+
+  
+//   return '*'.repeat(nFloors).split('').map((el, i) => `${' '.repeat(nFloors - 1 - i)}${'*'.repeat(nFloors - 1 - i)}${' '.repeat(nFloors - 1 - i)}`)
+    
 }
