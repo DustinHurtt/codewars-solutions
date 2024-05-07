eebf725e97edaadd63a27d634e0b836cec5ca801
Dustin Hurtt <dustin_hurtt@outlook.com>
2024-05-07 14:44:45 +0000

    Add: 8 kyu Even or Odd (53da3dbb4a5168369a0000fe)

---
+++ javascript/8kyu/even-or-odd.js
@@ -0,0 +1,9 @@
+// Codewars: Even or Odd
+// Link: https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+// Language: javascript
+// Kyu: 8 kyu
+function evenOrOdd(number) {
+  
+    return number % 2 === 0 ? "Even" : "Odd"
+  
+  }
